986d9bb7adaa50a83eb16eb449c31bc182984743
Felipenar-x <felipenaraki@gmail.com>
2026-04-27 09:06:58 -0300

    Delete app_livraria directory

--- app_livraria/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-def filtrar_livros(lista_livros, categoria_alvo):
-    """
-    Retorna apenas os livros da categoria escolhida.
-    """
-    return [livro for livro in lista_livros if livro["categoria"] == categoria_alvo]
